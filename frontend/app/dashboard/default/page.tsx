19ad27f407b49a22bc5d979fd91c7fe84bbf76a2
imad oissafe <oissafe.imad@gmail.com>
2025-01-02 20:27:48 +0100

    record video

--- frontend/app/dashboard/default/page.tsx
+++ frontend/app/dashboard/default/page.tsx
@@ -4,6 +4,7 @@ import { generateMeta } from "@/lib/utils";
 import MetricCard from "./cards/metric";
 import SubscriptionsCard from "./cards/subscriptions";
 import TotalRevenueCard from "./cards/total-revenue";
+import { currentUser } from "@clerk/nextjs/server";
 
 export async function generateMetadata() {
   return generateMeta({
@@ -14,23 +15,31 @@ export async function generateMetadata() {
   });
 }
 
-export default function Page() {
+export default async function Page() {
+  const user = await currentUser();
+
   return (
     <>
       <div className="mb-4 flex items-center justify-between space-y-2">
         <h1 className="text-2xl font-bold tracking-tight">Dashboard</h1>
-        <div className="flex items-center space-x-2">
-          <CalendarDateRangePicker />
-          <Button>Download</Button>
-        </div>
+        {user?.unsafeMetadata.favoriteProducts !== undefined && (
+          <div className="flex items-center space-x-2">
+            <CalendarDateRangePicker />
+            <Button>Download</Button>
+          </div>
+        )}
       </div>
-      <div className="grid lg:grid-cols-2">
-        <SubscriptionsCard />
-        <TotalRevenueCard />
-        <div className="lg:col-span-3">
-          <MetricCard className="h-full" />
+      {user?.unsafeMetadata.favoriteProducts !== undefined ? (
+        <div className="grid lg:grid-cols-2">
+          <SubscriptionsCard />
+          <TotalRevenueCard />
+          <div className="lg:col-span-3">
+            <MetricCard className="h-full" />
+          </div>
         </div>
-      </div>
+      ) : (
+        "You don't have any activity yet."
+      )}
     </>
   );
 }
